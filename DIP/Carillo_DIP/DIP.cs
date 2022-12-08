8ecb568e095b806d597d4f9449a802ca698b8ebd
Alys Anthea Carillo <111732588+AlysCarillo@users.noreply.github.com>
2022-12-08 19:25:03 +0800

    Add files via upload

---
+++ DIP/Carillo_DIP/DIP.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Carillo_DIP
+{
+    public partial class DIP : Form
+    {
+        Bitmap loaded, processed, background;
+        public DIP()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void dIP1ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            loaded = new Bitmap(openFileDialog1.FileName);
+            pictureBox1.Image = loaded;
+        }
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            pictureBox3.Image = processed;
+            processed.Save(saveFileDialog1.FileName);
+        }
+
+        private void greyscaleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            processed = new Bitmap(loaded.Width, loaded.Height);
+            for (int i = 0; i < loaded.Width; i++)
+            {
+                for (int j = 0; j < loaded.Height; j++)
+                {
+                    Color pixel = loaded.GetPixel(i, j);
+                    int grey = (pixel.R + pixel.G + pixel.B) / 3;
+                    processed.SetPixel(i, j, Color.FromArgb(grey, grey, grey));
+                }
+            }
+            pictureBox3.Image = processed;
+
+        }
+
+        private void basicCopyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            processed = new Bitmap(loaded.Width, loaded.Height);
+            for (int x = 0; x < loaded.Width; x++)
+            {
+                for (int y = 0; y < loaded.Height; y++)
+                {
+                    Color pixel = loaded.GetPixel(x, y);
+                    processed.SetPixel(x, y, pixel);
+                }
+
+            }
+            pictureBox3.Image = processed;
+        }
+
+        private void colorInversionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            processed = new Bitmap(loaded.Width, loaded.Height);
+            for (int i = 0; i < loaded.Width; i++)
+            {
+                for (int j = 0; j < loaded.Height; j++)
+                {
+                    Color pixel = loaded.GetPixel(i, j);
+                    processed.SetPixel(i, j, Color.FromArgb(255-pixel.R, 255-pixel.G, 255-pixel.B));
+                }
+            }
+            pictureBox3.Image = processed;
+        }
+
+        private void histogramToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Color sample;
+            Color gray;
+            Byte graydata;
+            //Grayscale Convertion;
+            for (int x = 0; x < loaded.Width; x++)
+            {
+                for (int y = 0; y < loaded.Height; y++)
+                {
+                    sample = loaded.GetPixel(x, y);
+                    graydata = (byte)((sample.R + sample.G + sample.B) / 3);
+                    gray = Color.FromArgb(graydata, graydata, graydata);
+                    loaded.SetPixel(x, y, gray);
+                }
+            }
+
+            //histogram 1d data;
+            int[] histdata = new int[256]; // array from 0 to 255
+            for (int x = 0; x < loaded.Width; x++)
+            {
+                for (int y = 0; y < loaded.Height; y++)
+                {
+                    sample = loaded.GetPixel(x, y);
+                    histdata[sample.R]++; // can be any color property r,g or processed
+                }
+            }
+
+            // Bitmap Graph Generation
+            // Setting empty Bitmap with background color
+            processed = new Bitmap(256, 800);
+            for (int x = 0; x < 256; x++)
+            {
+                for (int y = 0; y < 800; y++)
+                {
+                    processed.SetPixel(x, y, Color.White);
+                }
+            }
+            // plotting points based from histdata
+            for (int x = 0; x < 256; x++)
+            {
+                for (int y = 0; y < Math.Min(histdata[x] / 5, processed.Height - 1); y++)
+                {
+                    processed.SetPixel(x, (processed.Height - 1) - y, Color.Black);
+                }
+            }
+
+            pictureBox3.Image = processed;
+        }
+
+        private void sepiaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            processed = new Bitmap(loaded.Width, loaded.Height);
+            //color sepia
+            for (int i = 0; i < loaded.Width; i++)
+            {
+                for (int j = 0; j < loaded.Height; j++)
+                {
+                    Color pixel = loaded.GetPixel(i, j);
+                    int red = (int)(pixel.R * .393 + pixel.G * .769 + pixel.B * .189);
+                    int green = (int)(pixel.R * .349 + pixel.G * .686 + pixel.B * .168);
+                    int blue = (int)(pixel.R * .272 + pixel.G * .534 + pixel.B * .131);
+                    if (red > 255)
+                    {
+                        red = 255;
+                    }
+                    if (green > 255)
+                    {
+                        green = 255;
+                    }
+                    if (blue > 255)
+                    {
+                        blue = 255;
+                    }
+                    processed.SetPixel(i, j, Color.FromArgb(red, green, blue));
+                }
+            }
+            pictureBox3.Image = processed;
+        }
+
+        private void openFileDialog2_FileOk(object sender, CancelEventArgs e)
+        {
+            loaded = new Bitmap(openFileDialog2.FileName);
+            pictureBox1.Image = loaded;
+        }
+
+        private void openFileDialog3_FileOk(object sender, CancelEventArgs e)
+        {
+            background = new Bitmap(openFileDialog3.FileName);
+            pictureBox2.Image = background;
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            openFileDialog3.ShowDialog();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog2.ShowDialog();
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            processed = new Bitmap(loaded.Width, loaded.Height);
+            Color mygreen = Color.FromArgb(0, 0, 255);
+            int greygreen = (mygreen.R + mygreen.G + mygreen.B) / 3;
+            int threshold = 5;
+
+            for (int i = 0; i < loaded.Width; i++)
+            {
+                for (int j = 0; j < loaded.Height; j++)
+                {
+                    Color pixel = loaded.GetPixel(i, j);
+                    Color backPixel = background.GetPixel(i, j);
+                    int grey = (pixel.R + pixel.G + pixel.B) / 3;
+                    int subtractValue = Math.Abs(grey - greygreen);
+                    if (subtractValue < threshold)
+                    {
+                        processed.SetPixel(i, j, backPixel);
+                    }
+                    else
+                    {
+                        processed.SetPixel(i, j, pixel);
+                    }
+                }
+            }
+
+            pictureBox3.Image = processed;
+        }
+
+    }
+}
